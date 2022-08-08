17adc29228fb3d797a0338b0b7eca4404486ca28
Snorman <yuanmingze003@gmail.com>
2022-08-08 15:34:29 +0800

    Initial Commit

---
+++ LittlePink-git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittlePink-git
+//
+//  Created by Norman Yuan on 8/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LittlePink-gitUITests/LittlePink_gitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LittlePink_gitUITestsLaunchTests.swift
+//  LittlePink-gitUITests
+//
+//  Created by Norman Yuan on 8/8/22.
+//
+
+import XCTest
+
+class LittlePink_gitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
